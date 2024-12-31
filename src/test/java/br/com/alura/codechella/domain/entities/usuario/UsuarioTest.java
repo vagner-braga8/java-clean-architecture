0bf8c532d4e61e27508d02d3addb428da564ff34
Vagner <haigorne@gmail.com>
2024-12-31 09:59:06 -0300

    [03x02] Conceituando casos de uso e interfaces
    Abstraindo a persistência

--- src/test/java/br/com/alura/codechella/domain/entities/usuario/UsuarioTest.java
+++ src/test/java/br/com/alura/codechella/domain/entities/usuario/UsuarioTest.java
@@ -34,4 +34,15 @@ public class UsuarioTest {
         Assertions.assertEquals(email, usuario.getEmail());
     }
 
+    @Test
+    public void deveCriarUsuarioUsandoFabricaDeUsuario(){
+        FabricaDeUsuario fabrica = new FabricaDeUsuario();
+        Usuario usuario = fabrica.comNomeCpfNascimento("User 3", "555.143.785-16",LocalDate.parse("2007-09-08"));
+
+        Assertions.assertEquals("User 3", usuario.getNome());
+
+        usuario = fabrica.incluiEndereco("88017-180", 54, "apto 201");
+
+        Assertions.assertEquals("apto 201", usuario.getEndereco().getComplemento());
+    }
 }
